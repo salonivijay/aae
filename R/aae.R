d84265a39bad85f0b788c204efcf444abafbd6d5
salonivijay <salonivijay9@gmail.com>
2023-06-08 16:14:20 +0200

    Create package

---
+++ R/aae.R
@@ -0,0 +1,14 @@
+#' AAE values in Blantyre, Malawi
+#'
+#' The dataset consist of Black carbon concentrations in the breathing zone in Blantyre, Malawi. The Absorption angstrom exponent
+#' is calculated using the Black carbon concentrations at the two wavelengths.
+#'
+#' @format A dataframe with 4 variables and 341 observations
+#'
+#' \describe{
+#'   \item{time-malawi}{Local time in Malawi}
+#'   \item{bc-blue}{Black carbon concentration at infra-red wavelength light [ng/m3]}
+#'   \item{bc-ir}{Black carbon concentration at blue wavelength light [ng/m3]}
+#'   \item{aae-blue-ir}{Absorption angstrom exponent at Blue/IR wavelength}
+#' }
+#'
--- data-raw/data_processing.R
+++ data-raw/data_processing.R
@@ -1,3 +1,10 @@
+library(readr)
+
+aae <- read_csv("data-raw/example-bc-aae-data.csv")
+
 ## code to prepare `DATASET` dataset goes here
 
-usethis::use_data(DATASET, overwrite = TRUE)
+
+
+
+usethis::use_data(aae, overwrite = TRUE)
